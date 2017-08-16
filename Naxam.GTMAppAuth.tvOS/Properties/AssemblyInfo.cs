f591f37eff6011e3bfef643c3aa51c4a425857b3
Ngô Thanh Sơn <son.ngothanh@naxam.net>
2017-08-16 15:57:31 +0700

    Nuget package

--- Naxam.GTMAppAuth.tvOS/Properties/AssemblyInfo.cs
+++ Naxam.GTMAppAuth.tvOS/Properties/AssemblyInfo.cs
@@ -13,19 +13,19 @@ using Foundation;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Naxam.GTMAppAuth.tvOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin Binding Library - GTMAppAuth")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.6.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- Naxam.GTMSessionFetcher.tvOS/Properties/AssemblyInfo.cs
+++ Naxam.GTMSessionFetcher.tvOS/Properties/AssemblyInfo.cs
@@ -14,23 +14,22 @@ using ObjCRuntime;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Naxam.GTMSessionFetcher.tvOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin Binding Library - GTMSessionFetcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.11")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-[assembly: LinkWith("libGTMSessionFetcher.a", SmartLink = true, ForceLoad = true)]
